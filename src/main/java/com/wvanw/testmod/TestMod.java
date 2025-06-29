74a5efc664bd83f01c245b405648ca48bf39f690
Wessel van Weezep <wvanweezep@gmail.com>
2025-06-29 18:41:03 +0200

    Added Mod Item Setup

--- src/main/java/com/wvanw/testmod/TestMod.java
+++ src/main/java/com/wvanw/testmod/TestMod.java
@@ -1,17 +1,39 @@
 package com.wvanw.testmod;
 
+import com.wvanw.testmod.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
+import net.fabricmc.fabric.api.networking.v1.PacketByteBufs;
+import net.minecraft.item.BowItem;
+import net.minecraft.item.Items;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.Vec3d;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class TestMod implements ModInitializer {
+
+	public static final Identifier KILL_ENTITY_PACKET = Identifier.of("testmod", "kill_entity");
 
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
+
+		AttackEntityCallback.EVENT.register(((player, world, hand, entity, entityHitResult) -> {
+
+			if (player.getMainHandStack().isEmpty()) {
+				entity.addVelocity(player.getFacing().getDoubleVector().multiply(-5));
+			}
+			return ActionResult.PASS;
+		}));
 	}
 }
---
+++ src/main/java/com/wvanw/testmod/item/ModItems.java
@@ -0,0 +1,31 @@
+package com.wvanw.testmod.item;
+
+import com.wvanw.testmod.TestMod;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.component.type.FoodComponent;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.util.Identifier;
+
+public class ModItems {
+
+    public static final Item TEST_ITEM = registerItem("test_item");
+
+    private static Item registerItem(String name) {
+        Item item = new Item(new Item.Settings().food(new FoodComponent.Builder().build()).registryKey(
+                RegistryKey.of(RegistryKeys.ITEM, Identifier.of(TestMod.MOD_ID,name))));
+        return Registry.register(Registries.ITEM, Identifier.of(TestMod.MOD_ID, name), item);
+    }
+
+    public static void registerModItems() {
+        TestMod.LOGGER.info("Registering Mod Items for " + TestMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
+            entries.add(TEST_ITEM);
+        });
+    }
+}
